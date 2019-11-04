7362d57aba4b24552d0a91adfd9bdc5f3c6574f2
Timu5 <timusziomek@gmail.com>
2019-11-04 22:40:12 +0100

    Add map to flutter app

--- flutter/mobilna_politechnika/lib/Map.dart
+++ flutter/mobilna_politechnika/lib/Map.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:mobilna_politechnika/MyDrawer.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class Map extends StatelessWidget {
+class Map extends StatefulWidget {
+  @override
+  _MapState createState() => new _MapState();
+}
+
+class _MapState extends State<Map> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Mapa"),
-      ),
-      drawer: MyDrawer(),
-      body: Center(
-
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Mapa"),
+        ),
+        drawer: MyDrawer(),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                    child: WebView(
+                  initialUrl: 'http://mojmegatestkolejny.azurewebsites.net/nav/',
+                  javascriptMode: JavascriptMode.unrestricted,
+                  onPageFinished: (String url) {
+                    print('Page finished loading: $url');
+                  },
+                )),
+              ),
+            ],
+          ),
+        ));
   }
 }

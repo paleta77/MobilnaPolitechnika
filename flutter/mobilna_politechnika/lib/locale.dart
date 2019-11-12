8d0992f9d65b819167646da4d0f5f233263d69a1
Timu5 <timusziomek@gmail.com>
2019-11-12 18:05:06 +0100

    Add localization support

---
+++ flutter/mobilna_politechnika/lib/locale.dart
@@ -0,0 +1,55 @@
+class Locale {
+  static Map<String, String> current = pl;
+
+  static Map<String, String> pl = {
+    'login_text': 'Logowanie',
+    'login_button': 'Zaloguj',
+    'login': 'Login',
+    'password': 'Hasło',
+    'enter_login': 'Wpisz login!',
+    'enter_password': 'Wpisz hasło!',
+    'error': 'Błąd',
+    'login_error':
+        'Wystąpił problem podczas proby zalogowania, sprawdz dane logowania.',
+    'close': 'Zamknij',
+    'profile': 'Profil',
+    'grades': 'Oceny',
+    'map': 'Mapa',
+    'schedule': 'Plan zajęć',
+    'logout': 'Wyloguj',
+    'average': 'Średnia: ',
+    'monday': 'Poniedziałek',
+    'tuesday': 'Wtorek',
+    'wednesday': 'Środa',
+    'thursday ': 'Czwartek',
+    'friday ': 'Piątek',
+    'saturday': 'Sobota',
+    'sunday': 'Niedziela'
+  };
+
+  static Map<String, String> en = {
+    'login_text': 'Login',
+    'login_button': 'Login',
+    'login': 'Username',
+    'password': 'Password',
+    'enter_login': 'Enter username!',
+    'enter_password': 'Enter password!',
+    'error': 'Error',
+    'login_error':
+        'Error occurs when trying tp login. Please check your credentials.',
+    'close': 'Close',
+    'profile': 'Profile',
+    'grades': 'Grades',
+    'map': 'Map',
+    'schedule': 'Schedule',
+    'logout': 'Logout',
+    'average': 'Average: ',
+    'monday': 'Monday',
+    'tuesday': 'Tuesday',
+    'wednesday': 'Wednesday',
+    'thursday ': 'Thursday',
+    'friday ': 'Friday',
+    'saturday': 'Saturday',
+    'sunday': 'Sunday'
+  };
+}

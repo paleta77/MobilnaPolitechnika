752041436d0bc665192670c4158c40add23782c3
Timu5 <timusziomek@gmail.com>
2019-11-20 19:34:16 +0100

    Add graphics to easter egg

--- flutter/mobilna_politechnika/lib/easter.dart
+++ flutter/mobilna_politechnika/lib/easter.dart
@@ -1,7 +1,9 @@
+import 'package:flame/position.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flame/game.dart';
 import 'package:flame/gestures.dart';
+import 'package:flame/sprite.dart';
 import 'dart:math';
 
 class Easter extends StatefulWidget {
@@ -21,19 +23,30 @@ class _EasterState extends State<Easter> {
 
 class EasterGame extends Game with TapDetector {
   Size screenSize;
-  double y = 0;
-  double speed = 0;
+  double y;
+  double speed;
   Random rand = Random();
-  int points = 0;
-  bool started = false;
-  bool dead = false;
+  int points;
+  bool started;
+  bool dead;
 
-  var tubePos = [200.0, 400.0, 600.0];
-  var tubeHeight = [0.0, -10.0, 10.0];
+  List<double> tubePos;
+  List<double> tubeHeight;
+
+  var bgSprite = Sprite('spritesheet.png', width: 144, height: 255);
+  var playerSprite = Sprite('spritesheet.png', x: 144, width: 17, height: 12);
+  var playerSprite1 =
+      Sprite('spritesheet.png', x: 144 + 17.0, width: 17, height: 12);
+  var playerSprite2 =
+      Sprite('spritesheet.png', x: 144 + 17.0 * 2, width: 17, height: 12);
+  var tubeUpSpirite =
+      Sprite('spritesheet.png', x: 144, y: 36, width: 26, height: 160);
+  var tubeDownSpirite =
+      Sprite('spritesheet.png', x: 144 + 26.0, y: 36, width: 26, height: 160);
 
   void resize(Size size) {
     screenSize = size;
-    print(screenSize);
+    reset();
     super.resize(size);
   }
 
@@ -41,16 +54,27 @@ class EasterGame extends Game with TapDetector {
     Rect rect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
     Paint paint = Paint();
     paint.color = Color(0xff000000);
-
     canvas.drawRect(rect, paint);
+    bgSprite.render(canvas, width: screenSize.width, height: screenSize.height);
 
     double screenCenterX = screenSize.width / 2;
     double screenCenterY = screenSize.height / 2;
     Rect player =
-        Rect.fromLTWH(screenCenterX - 20, screenCenterY - 20 + y, 40, 40);
+        Rect.fromLTWH(screenCenterX - 20, screenCenterY - 20 + y, 30, 30);
 
     paint.color = Color(0xffffffff);
-    canvas.drawRect(player, paint);
+    //canvas.drawRect(player, paint);
+    if (speed < -100)
+      playerSprite2.renderPosition(
+          canvas, Position(player.left - 5, player.top),
+          size: Position(40, 30));
+    else if (speed < 0)
+      playerSprite1.renderPosition(
+          canvas, Position(player.left - 5, player.top),
+          size: Position(40, 30));
+    else
+      playerSprite.renderPosition(canvas, Position(player.left - 5, player.top),
+          size: Position(40, 30));
 
     for (int i = 0; i < tubePos.length; i++) {
       var tube = tubePos[i];
@@ -58,18 +82,22 @@ class EasterGame extends Game with TapDetector {
 
       Rect tube1 = Rect.fromLTWH(
           tube, screenCenterY + 75 + height, 50, screenCenterY - 75 - height);
-      canvas.drawRect(tube1, paint);
-
+      //canvas.drawRect(tube1, paint);
+      tubeDownSpirite.renderPosition(canvas, Position(tube1.left, tube1.top),
+          size: Position(tube1.width, tube1.height));
+     
       Rect tube2 = Rect.fromLTWH(tube, 0, 50, screenCenterY - 75 + height);
-      canvas.drawRect(tube2, paint);
+      //canvas.drawRect(tube2, paint);
+      tubeUpSpirite.renderPosition(canvas, Position(tube2.left, tube2.top),
+          size: Position(tube2.width, tube2.height));
 
       Rect inter1 = tube1.intersect(player);
       Rect inter2 = tube2.intersect(player);
       if ((inter1.width > 0 && inter1.height > 0) ||
           (inter2.width > 0 && inter2.height > 0)) {
-            dead = true;
-            started = false;
-          }
+        dead = true;
+        started = false;
+      }
     }
 
     final TextPainter textPainter = TextPainter(
@@ -101,7 +129,7 @@ class EasterGame extends Game with TapDetector {
   }
 
   void onTapDown(TapDownDetails d) {
-    if(dead)
+    if (dead)
       reset();
     else
       started = true;

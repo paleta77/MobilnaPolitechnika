72b7042781a93c1571f361a4cef2d4bb45b80305
agnieszka <baga16@o2.pl>
2019-12-08 21:26:34 +0100

    Improve grades layout in app

--- flutter/mobilna_politechnika/lib/grades.dart
+++ flutter/mobilna_politechnika/lib/grades.dart
@@ -7,7 +7,7 @@ import 'side-drawer.dart';
 class Grades extends StatefulWidget {
   @override
   _DisplayGradeState createState() {
-    return _DisplayGradeState();
+    return new _DisplayGradeState();
   }
 }
 
@@ -32,6 +32,7 @@ class _DisplayGradeState extends State {
 
     if (grades['msg'] != 'OK') {
       // problem :/
+      return;
     }
 
     grades = grades['grades'];
@@ -41,7 +42,9 @@ class _DisplayGradeState extends State {
     for (int i = 0; i < grades.length; i++) {
       var grade = grades[i];
       gradesList.add(GradeModel(
-          subject: grade['subject'], ects: grade['ects'].toString(), value: grade['value'].toString()));
+          subject: grade['subject'],
+          ects: grade['ects'].toString(),
+          value: grade['value'].toString()));
 
       sum += grade['value'];
     }
@@ -59,26 +62,87 @@ class _DisplayGradeState extends State {
     super.initState();
   }
 
+  void longPress(GradeModel grade) {
+    print("Long press " + grade.subject);
+    showDialog(
+        context: context,
+        builder: (context) {
+          return StatefulBuilder(
+            builder: (context, setState) {
+              return AlertDialog(
+                title: Text("Edytuj ocene"),
+                content: new Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Row(children: <Widget>[
+                        Expanded(child: Text("1")),
+                        Expanded(child: Center(child: Text("2"))),
+                        Expanded(child: Text("3"))
+                      ]),
+                      RaisedButton(
+                          child: Text("Usun", textAlign: TextAlign.center),
+                          onPressed: () {
+                            print("Remove " + grade.subject);
+                          }),
+                    ]),
+                actions: <Widget>[
+                  FlatButton(
+                    onPressed: () async {
+                      Navigator.pop(context);
+                    },
+                    child: Text("Anuluj"),
+                  )
+                ],
+              );
+            },
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text(Locale.current['grades'])),
         drawer: SideDrawer(),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              // Add your onPressed code here!
+            },
+            child: Text("+"),
+            backgroundColor: Color.fromARGB(255, 128, 1, 0)),
         body: Column(children: <Widget>[
           Expanded(
               child: ListView.builder(
             itemCount: gradeModelData.length,
-            itemBuilder: (context, int i) => Column(
-              children: [
-                new ListTile(
-                  title: new Text(gradeModelData[i].subject + " " + gradeModelData[i].ects),
-                  subtitle: new Text(gradeModelData[i].value),
-                  onTap: () {},
-                  onLongPress: () {},
-                ),
-              ],
-            ),
-          )),
+                  itemBuilder: (context, int i) => Card(
+                      child: InkWell(
+                          onLongPress: () => longPress(gradeModelData[i]),
+                          onTap: () {
+                            print("click " + gradeModelData[i].subject);
+                          },
+                          child: Container(
+                              height: 70,
+                              child: Row(children: <Widget>[
+                                Expanded(
+                                    child: Column(children: [
+                                  Expanded(
+                                      child: Center(
+                                          child:
+                                              Text(gradeModelData[i].subject))),
+                                  Expanded(
+                                      child: Center(
+                                          child: Text(
+                                              gradeModelData[i].ects + "ECTS",
+                                              style: TextStyle(
+                                                  color: Colors.grey))))
+                                ])),
+                                Expanded(
+                                    child: Center(
+                                        child: Text(gradeModelData[i].value,
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20))))
+                              ])))))),
           Container(
               height: 25,
               color: Color.fromARGB(255, 230, 230, 230),

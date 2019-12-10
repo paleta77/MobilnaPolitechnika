b8d29b521f0519e94f1630532c5188b85aec5e6a
Timu5 <timusziomek@gmail.com>
2019-12-10 18:36:31 +0100

    Add initial native flutter search to map

--- flutter/mobilna_politechnika/lib/map.dart
+++ flutter/mobilna_politechnika/lib/map.dart
@@ -13,11 +13,28 @@ class MapState extends State<Map> {
   WebViewController controller;
   static String target = "";
 
+  void openSearch(BuildContext context) {
+    showSearch(context: context, delegate: _MapSearchDelegate(this));
+  }
+
+  void setTarget(String text) {
+    target = text;
+    controller.evaluateJavascript('setTarget("$text")');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text(Locale.current['map']),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                openSearch(context);
+              },
+            )
+          ],
         ),
         drawer: SideDrawer(),
         body: Container(
@@ -38,6 +55,9 @@ class MapState extends State<Map> {
                         name: 'host',
                         onMessageReceived: (JavascriptMessage msg) {
                           print(msg.message);
+                          if (msg.message == "open_search") {
+                            openSearch(context);
+                          }
                         })
                   ]),
                   onPageFinished: (String url) {},
@@ -46,5 +66,108 @@ class MapState extends State<Map> {
             ],
           ),
         ));
+  }
+}
+
+class _MapSearchDelegate extends SearchDelegate<String> {
+  final List<String> _data = <String>[
+    "CTI 201/B19",
+    "CTI 202/B19",
+    "CTI 203/B19",
+    "CTI 204/B19",
+    "CTI 204/B19",
+    "CTI 101/B19"
+  ];
+  final MapState mapState;
+
+  _MapSearchDelegate(this.mapState);
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      tooltip: 'Back',
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final Iterable<String> suggestions = query.isEmpty
+        ? _data.take(10)
+        : _data.where((String i) => i.startsWith(query));
+
+    return _SuggestionList(
+      query: query,
+      suggestions: suggestions.toList(),
+      onSelected: (String suggestion) {
+        query = suggestion;
+        mapState.setTarget(suggestion);
+        close(context, suggestion);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return null;
+  }
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return <Widget>[
+      if (query.isNotEmpty)
+        IconButton(
+          tooltip: 'Clear',
+          icon: const Icon(Icons.clear),
+          onPressed: () {
+            query = '';
+            showSuggestions(context);
+          },
+        ),
+    ];
+  }
+}
+
+class _SuggestionList extends StatelessWidget {
+  const _SuggestionList({this.suggestions, this.query, this.onSelected});
+
+  final List<String> suggestions;
+  final String query;
+  final ValueChanged<String> onSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return ListView.builder(
+      itemCount: suggestions.length,
+      itemBuilder: (BuildContext context, int i) {
+        final String suggestion = suggestions[i];
+        return ListTile(
+          leading: const Icon(Icons.room),
+          title: RichText(
+            text: TextSpan(
+              text: suggestion.substring(0, query.length),
+              style:
+                  theme.textTheme.subhead.copyWith(fontWeight: FontWeight.bold),
+              children: <TextSpan>[
+                TextSpan(
+                  text: suggestion.substring(query.length),
+                  style: theme.textTheme.subhead,
+                ),
+              ],
+            ),
+          ),
+          onTap: () {
+            onSelected(suggestion);
+          },
+        );
+      },
+    );
   }
 }

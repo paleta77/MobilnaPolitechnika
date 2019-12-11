dd81d227b616ebee0cad21d08f1ee68dd6e60c4d
agnieszka <baga16@o2.pl>
2019-12-11 20:17:45 +0300

    Add initial Profil page
    Co-authored-by: Mateusz Muszyński <timusziomek@gmail.com>

--- flutter/mobilna_politechnika/lib/profile.dart
+++ flutter/mobilna_politechnika/lib/profile.dart
@@ -8,17 +8,79 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(Locale.current['profile']),
-      ),
-      drawer: SideDrawer(),
-      body: Center(
+        appBar: AppBar(
+          elevation: 0.0,
+        ),
+        drawer: SideDrawer(),
+        body: Column(children: <Widget>[
+          Container(
+            height: 200,
+            color: Color.fromARGB(255, 128, 1, 0),
+            child: Center(
+                child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 65,
+                  backgroundColor: Colors.white,
+                ),
+                Text(User.instance.name,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold)),
+              ],
+            )),
+          ),
+          Expanded(
+              child: SingleChildScrollView(
+                  child: Column(children: <Widget>[
+            Container(height: 90, child: Center(child: Text("mail"))),
+            Container(
+              height: 90,
+              child: RaisedButton(
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) {
+                        return StatefulBuilder(
+                          builder: (context, setState) {
+                            return AlertDialog(
+                              title: Text("Wybierz grupe"),
+                              content: new Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[],
+                              ),
+                              actions: <Widget>[
+                                FlatButton(
+                                  onPressed: () async {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Text("Zapisz"),
+                                )
+                              ],
+                            );
+                          },
+                        );
+                      });
+                },
+                child: Text("choose group"),
+              ),
+              color: Colors.amber,
+            ),
+            Container(
+              height: 90,
+              child: Center(child: Text("average")),
+            )
+          ])))
+        ])
+
+        /*Center(
           child: Text("name: " +
               User.instance.name +
               '\nmail: ' +
               User.instance.mail +
               '\ngroup: ' +
-              User.instance.group)),
-    );
+              User.instance.group)),*/
+        );
   }
 }

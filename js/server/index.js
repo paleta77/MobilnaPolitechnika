db348d5e87bac29040c52c351576fba3511c0cd3
Timu5 <timusziomek@gmail.com>
2019-12-14 17:21:45 +0100

    Add initial swagger support

--- js/server/index.js
+++ js/server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const sendgrid = require('@sendgrid/mail');
+const swagger = require('swagger-ui-express');
+const YAML = require('yamljs');
 const auth = require('./auth.js');
 
 const config = require('./config.js');
@@ -13,6 +15,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(auth.check);
+app.use('/api-docs', swagger.serve, swagger.setup(YAML.load('./swagger.yaml')));
 
 require('./routes.js')(app);
 
